4ec6659a5dd0adb4c22452d697838d48292c16f2
rinnothing <58170199+rinnothing@users.noreply.github.com>
2025-11-16 15:22:16 +0300

    bring everything together

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -2,9 +2,46 @@ package logger
 
 import (
 	"context"
+	"os"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
+
+func ConstructLogger(level, filepath string) (*zap.Logger, error) {
+	lvl, err := zapcore.ParseLevel(level)
+	if err != nil {
+		return nil, err
+	}
+
+	consoleEncoder := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
+
+	consoleCore := zapcore.NewCore(
+		consoleEncoder,
+		zapcore.Lock(os.Stdout),
+		lvl,
+	)
+
+	// returning early if file is not specified
+	if filepath == "" {
+		return zap.New(consoleCore, zap.AddCaller()), nil
+	}
+
+	file, _ := os.OpenFile(filepath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	fileEncoder := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
+
+	fileCore := zapcore.NewCore(
+		fileEncoder,
+		zapcore.Lock(file),
+		zapcore.DebugLevel,
+	)
+
+	core := zapcore.NewTee(consoleCore, fileCore)
+
+	logger := zap.New(core, zap.AddCaller())
+
+	return logger, nil
+}
 
 type key struct{}
 
